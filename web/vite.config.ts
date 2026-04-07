fa62d79dee8ea1e76c15da9b6a55e778c3577e0f
Alyson Vilela <ctt.alysonvilela@gmail.com>
2026-04-07 15:28:04 +0000

    fix: build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ env }) => {
-  const apiUrl = env.VITE_API_URL || ''
+export default defineConfig(() => {
+  const apiUrl = (process.env.VITE_API_URL || '').trim()
   const allowedHosts = apiUrl ? [new URL(apiUrl).hostname] : []
 
   return {
@@ -13,11 +13,12 @@ export default defineConfig(({ env }) => {
       allowedHosts,
     },
     plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }),
+      react(),
+      tailwindcss(),
+    ],
+  }
 })
